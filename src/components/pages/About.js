4da4e2b36f8e152ca7bf249a9f88f53fd4efbeb3
Albert <albertonly898@gmail.com>
2019-04-05 20:34:31 +0700

    First Commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="display-4">About Manager</h1>
+      <p className="lead">Simple app to manage contacts</p>
+      <p className="text-secondory">Version 1.0.0</p>
+    </div>
+  );
+}
+
+export default About;
