7b004d0188aab0b26333e437c9338e773f923053
Albert <albertonly898@gmail.com>
2019-04-07 20:43:53 +0700

    Using Async Await

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -14,7 +14,7 @@ class AddContact extends Component {
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
-  onSubmit = (dispatch, e) => {
+  onSubmit = async (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
@@ -41,9 +41,12 @@ class AddContact extends Component {
       phone
     };
 
-    axios
-      .post("https://jsonplaceholder.typicode.com/users", newContact)
-      .then(res => dispatch({ type: "ADD_CONTACT", payload: res.data }));
+    const res = await axios.post(
+      "https://jsonplaceholder.typicode.com/users",
+      newContact
+    );
+
+    dispatch({ type: "ADD_CONTACT", payload: res.data });
 
     // Clear State
     this.setState({
