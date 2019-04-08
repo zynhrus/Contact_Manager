93df2410f4f6d5aa65aa2ad95007deaa6042fef7
Albert <albertonly898@gmail.com>
2019-04-08 20:33:33 +0700

    Updates

---
+++ precache-manifest.10fabf51f632437a9b9fbedc918e626f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a97c62b636dc4983df8c",
+    "url": "/contact_manager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "a97c62b636dc4983df8c",
+    "url": "/contact_manager/static/js/main.3e00fafd.chunk.js"
+  },
+  {
+    "revision": "2b941c4b3b0d1aff9d5e",
+    "url": "/contact_manager/static/js/runtime~main.75798bfb.js"
+  },
+  {
+    "revision": "4a5a55808ce20eb9c442",
+    "url": "/contact_manager/static/css/2.aefb6f58.chunk.css"
+  },
+  {
+    "revision": "4a5a55808ce20eb9c442",
+    "url": "/contact_manager/static/js/2.97416df2.chunk.js"
+  },
+  {
+    "revision": "27ba68b1f09406cad7d2e5ae794c877b",
+    "url": "/contact_manager/index.html"
+  }
+];
